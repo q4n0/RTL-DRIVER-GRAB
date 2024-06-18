6896edfe13f7a0f245791bb518c1e90ad832ab02
byts3c <154826956+q4n0@users.noreply.github.com>
2024-06-18 04:26:50 +0300

    Update and rename rtlinstll.sh to debubrtlinstll.sh

--- myscripts/debubrtlinstll.sh
+++ myscripts/debubrtlinstll.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 sudo apt-get install make gcc linux-headers-$(uname -r) build-essential git
+sudo apt-get install make gcc linux-headers-$(uname -r) build-essential git
 git clone https://github.com/lwfinger/rtw88.git
 cd /rtw88
 make
