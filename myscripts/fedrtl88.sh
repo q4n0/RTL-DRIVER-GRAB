000d4190d4cbcd2999706a57a7fe8105ba4ff2b4
byts3c <154826956+q4n0@users.noreply.github.com>
2024-06-18 04:59:32 +0300

    Update fedrtl88.sh

--- myscripts/fedrtl88.sh
+++ myscripts/fedrtl88.sh
@@ -1,6 +1,6 @@
 sudo dnf install kernel-headers kernel-devel
 sudo dnf group install "C Development Tools and Libraries"
 git clone https://github.com/lwfinger/rtw88.git
-cd /rtw88
+cd /RTL-DRIVER-GRAB/myscripts/rtw88
 make
 sudo make install
